c0772b5d7b8de0c8b585a533d2b8f39410c42b4c
Ferric <zhangkai8888888888@gmail.com>
2019-03-19 22:57:43 +0800

    Init with webpack boilerplate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button @click="visible = true">Button</el-button>
+    <el-dialog :visible.sync="visible" title="Hello world">
+      <p>{{ msg }}</p>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      visible: false,
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
